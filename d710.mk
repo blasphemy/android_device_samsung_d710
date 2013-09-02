aab4d5c91f98f35deba337cf05d2179c54af1395
Dorian Snyder <dastin1015@gmail.com>
2013-09-01 19:19:39 -0700

    Revert "d710: add opensource libril"
    This reverts commit a71020517ac5d36933cb33859ea9c6f3e52df73c.

--- d710.mk
+++ d710.mk
@@ -110,6 +110,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.wimax.interface=uwbr0 \
     net.connectivity.type=CDMA1 \
     mobiledata.interfaces=ppp0,wlan0,uwbr0 \
+    ro.telephony.ril_class=SamsungCDMAv6RIL \
     ro.ril.samsung_cdma=true \
     ro.carrier=Sprint
 
